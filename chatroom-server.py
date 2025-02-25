9fd340fafb4226b5c7346b231a340e4f1efc3362
Matthew Kruse <kruse.matthew2@gmail.com>
2025-02-24 22:19:18 -0600

    server client init commit

---
+++ chatroom-server.py
@@ -0,0 +1,70 @@
+import socket
+import threading
+
+# TODO:  Build "group chats" so that users can send and receive to a group of users.
+#  Allow for non-text messages, such as file attachments.
+# Other things too...
+
+# List to keep track of connected client sockets
+clients = []
+
+def listen():
+    server_name = '10.0.16.1'
+    server_port = 10164
+
+    # open a socket and listen for client connections
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((server_name, server_port))
+    server.listen()
+    print(f"Server: Listening on {server_name}:{server_port}")
+
+    while True:
+        # accept any incoming sockets & append client to list
+        client_socket, addr = server.accept()
+        clients.append(client_socket)
+
+        # create a new thread for each client connection
+        thread = threading.Thread(target=message_listener_thread, args=(client_socket, addr))
+        thread.start()
+        print(f"Active Connections: {threading.active_count() - 1}")
+
+
+def message_listener_thread(client_socket, addr):
+    # listen for messages on client socket
+    print(f"Client: {addr} connected.")
+    while True:
+        try:
+            # receive message from client
+            message = client_socket.recv(1024)
+            if not message:
+                # client is gone
+                break
+            # output message to console
+            print(f"[{addr}] {message.decode('utf-8')}")
+
+            # send message
+            send_message(message, client_socket)
+        except Exception as e:
+            print(f"Error with {addr}: {e}")
+            break
+
+    print(f"Client: {addr} disconnected.")
+
+    clients.remove(client_socket)
+    client_socket.close()
+
+def send_message(message, sender_socket):
+    # loop through all clients
+    for client in clients:
+
+        # send to everyone but skip the message sender
+        if client != sender_socket:
+            try:
+                client.send(message)
+            except Exception as e:
+                print(f"Error sending message: {e}")
+                clients.remove(client)
+
+
+if __name__ == "__main__":
+    listen()
---
+++ test-client.py
@@ -0,0 +1,58 @@
+import socket
+import threading
+
+# TODO: Use threading for the input listener
+
+def connect():
+    server_name = '10.0.16.1'
+    server_port = 10164
+
+    # create a socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client_socket.connect((server_name, server_port))
+    except Exception as e:
+        print(f"Connection failed reaching {server_name}:{server_port} \n {e}")
+        return
+
+    print("Chat connection successful. Type 'quit' to exit.")
+
+    # open a thread to listen for server messages
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread.daemon = True
+    receive_thread.start()
+
+    # Start thread to send messages to the server (or run in main thread)
+    # do we need this?
+    process_inputs(client_socket)
+
+def receive_messages(client_socket):
+    # continuously print messages from server
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            if not message:
+                print("Disconnected from server")
+                break
+            print(message.decode('utf-8'))
+        except Exception as e:
+            print(f"Error receiving message: {e}")
+            break
+
+def process_inputs(client_socket):
+    # continuously listen for console input and send
+    while True:
+        message = input("")
+
+        # close socket(s) on the server side upon user 'quit'
+        if message.lower() == 'quit':
+            client_socket.close()
+            break
+        try:
+            client_socket.send(message.encode('utf-8'))
+        except Exception as e:
+            print(f"Error sending message: {e}")
+            break
+
+if __name__ == "__main__":
+    connect()
